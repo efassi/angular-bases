6f11e035db120e8523f0d8c828975742ba732163
efassi <emilianofassi@gmail.com>
2023-08-23 10:42:25 -0300

    Modulo add character

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dbz-add-caracter',
+  selector: 'dbz-add-caracter',
   templateUrl: './add-character.component.html',
   styleUrls: ['./add-character.component.css']
 })
